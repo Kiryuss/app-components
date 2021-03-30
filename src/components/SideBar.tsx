10a6965245192eb7190c80e066c436a6e3c9918e
Guilherme <guilhermesbw2@gmail.com>
2021-03-29 22:03:33 -0300

    Finalizado

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,52 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from './Button';
+import { useState, useEffect } from 'react'
+import { api } from '../services/api';
+import '../styles/sidebar.scss';
+
+interface propsType {
+  childData: Function;
+}
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+export function SideBar({ childData } : propsType) {
+  const [selectedGenreId, setSelectedGenreId] = useState(1);
+  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+  
+  function handleClickButton(id: number) {
+    setSelectedGenreId(id);
+    childData(selectedGenreId);
+  }
+  
+  useEffect(() => {
+    api.get<GenreResponseProps[]>('genres').then(response => {
+      setGenres(response.data);
+    });
+  }, []);
+
+
+ 
+
+  return(
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+  </div>
+
+</nav>
+  )
+  
 }
